3cb8b1e795ed4102d24ef9aba3dc7f992905b079
tpthanh2006 <tranpthanh2006@gmail.com>
2025-02-16 11:55:31 -0600

    learn some node basics

--- node-basics/app.js
+++ node-basics/app.js
@@ -2,4 +2,11 @@ const greet = (name) => {
   console.log(name);
 };
 
-greet("William");
+//greet("William");
+
+/* globalThis.setInterval(() => {
+  console.log("Inventory Pilot");
+}, 1000) */
+
+console.log(__dirname);
+console.log(__filename);
---
+++ node-basics/person.js
@@ -0,0 +1,5 @@
+const person = "William"
+const people = ["William", "Nam", "Tri"]
+
+console.log(person);
+console.log(people);
