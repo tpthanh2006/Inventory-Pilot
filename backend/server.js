7a1679d29a7eaa6715269482cf691d1426b2f646
tpthanh2006 <tranpthanh2006@gmail.com>
2025-02-19 17:13:13 -0600

    project setup with mongo db

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    })
+  })
+  .catch((err) => console.log(err))
+
--- node-basics/express-server.js
+++ node-basics/express-server.js
@@ -5,7 +5,11 @@ const app = express()
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("<h2> Welcome to Home Page (Express.JS) </h2>")
+  res.send("<h2>Welcome to Home Page (Express.JS)</h2>")
+})
+
+app.get("/about", (req, res) => {
+  res.send("<h2> Welcome to the About Page</h2>")
 })
 
 app.listen("5000", () => {
