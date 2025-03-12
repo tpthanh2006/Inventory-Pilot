c0472b2b056af9188f2231ac37c59669b36dde0b
tpthanh2006 <tranpthanh2006@gmail.com>
2025-03-12 17:57:59 -0500

    get user data testing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -131,8 +131,28 @@ const logoutUser = asyncHandler( async(req, res) => {
   });
 });
 
+// Get User Data
+const getUser = asyncHandler( async(req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+      const { _id, name, email, photo, phone, bio } = user;
+      res.status(200).json({
+        _id,
+        name,
+        email,
+        photo,
+        phone,
+        bio
+      });
+  } else {
+    res.status(400);
+    throw new Error("User not found");    
+  }
+});
+
 module.exports = { 
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  getUser
 };
