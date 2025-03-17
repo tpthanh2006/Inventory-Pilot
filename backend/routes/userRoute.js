91122e35c9e69cb38289f727ad01d12093fc6bb3
tpthanh2006 <tranpthanh2006@gmail.com>
2025-03-16 19:55:54 -0500

    set up update user route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser, getUser } = require("../controllers/userController");
+const { registerUser, loginUser, logoutUser, getUser, loginStatus, updateUser } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logoutUser);
 router.get("/getuser", protect, getUser);
+router.get("/loggedin", loginStatus);
+router.patch("/updateuser", protect, updateUser);
 
 module.exports = router;
