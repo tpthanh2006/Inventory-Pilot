a69e0ea4bf76dcb9f4eca438a7524fc95bf5dfc9
tpthanh2006 <tranpthanh2006@gmail.com>
2025-03-27 21:26:36 -0500

    finish change pw controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -196,7 +196,32 @@ const updateUser = asyncHandler ( async(req, res) => {
 
 // Change Password
 const changePassword = asyncHandler ( async(req, res) => {
-  res.send("change pw");
+  const user = await User.findById(req.user._id);
+
+  const {oldPassword, password} = req.body;
+
+  // Validate
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, please sign up");
+  }
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please add both old and new password");
+  }
+
+  // Check if old password matches in DB
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new password
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send("Password changed successful");
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
+  }
 });
 
 module.exports = { 
