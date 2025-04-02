71d1ea905c9fe31f35479577a26a04f770d6b96c
tpthanh2006 <tranpthanh2006@gmail.com>
2025-04-02 17:36:56 -0500

    finish setting up create a new product

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
+
+const createProduct = asyncHandler (async(req, res) => {
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  // Validation
+  if (!name || !category || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  // Manage Image Upload
+
+  // Create Product
+  const product = await Product.create({
+    user: req.user.id,
+    name,
+    sku,
+    category,
+    quantity,
+    price,
+    description
+  })
+
+  res.status(201).json(product);
+});
+
+module.exports = {
+  createProduct,
+}
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+
 const app = express();
 const errorHandler = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
@@ -16,6 +18,7 @@ app.use(bodyParser.json());
 
 // Routes Middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // Routes 
 app.get("/", (req, res) => {
