1a3499f96877aca1d5073b07475f7cfa470c5627
tpthanh2006 <tranpthanh2006@gmail.com>
2025-04-04 22:01:40 -0500

    set up get all products with images uploaded to cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
+const cloudinary = require("cloudinary").v2;
 
 // Create a Product
 const createProduct = asyncHandler (async(req, res) => {
@@ -15,9 +16,24 @@ const createProduct = asyncHandler (async(req, res) => {
   // Handle File Upload
   let fileData = {};
   if (req.file) {
+    // Save image to Cloudinary
+    let uploadedFile;
+    try {
+      uploadedFile = await cloudinary.uploader.upload(
+        req.file.path,
+        {
+          folder: "Inventory Pilot",
+          resource_type: "image"
+        }
+      )
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
     fileData = {
       fileName: req.file.originalname,
-      filePath: req.file.path,
+      filePath: uploadedFile.secure_url,
       fileType: req.file.mimetype,
       fileSize: fileSizeFormatter(req.file.size, 2),
     }
@@ -38,6 +54,16 @@ const createProduct = asyncHandler (async(req, res) => {
   res.status(201).json(product);
 });
 
+// Get All Products
+const getProducts = asyncHandler(async(req, res) => {
+  const products = await Product
+    .find({ user: req.user.id })
+    .sort("-createdAt");
+    
+  res.status(200).json(products);
+});
+
 module.exports = {
   createProduct,
+  getProducts
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { upload } = require("../utils/fileUpload");
 const protect = require("../middleware/authMiddleware");
-const { createProduct } = require("../controllers/productController");
+const { createProduct, getProducts } = require("../controllers/productController");
 const router = express.Router();
 
 router.post("/", protect, upload.single("image"), createProduct);
+router.get("/", protect, getProducts);
 
 module.exports = router;
