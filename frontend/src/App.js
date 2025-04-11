85b23acc1f996be60e586b886ad198d5cb14fd27
tpthanh2006 <tranpthanh2006@gmail.com>
2025-04-10 20:56:00 -0500

    finish sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Forgot from "./pages/auth/Forgot";
 import Reset from "./pages/auth/Reset";
-
+import Forgot from "./pages/auth/Forgot";
+import Register from "./pages/auth/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Sidebar from "./components/sidebar/Sidebar";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
@@ -15,6 +17,13 @@ function App() {
         <Route path="/register" element={<Register/>} />
         <Route path="/forgot" element={<Forgot/>} />
         <Route path="/resetpassword/:resetToken" element={<Reset/>} />
+        <Route path="/dashboard" element={
+          <Sidebar>
+            <Layout>
+              <Dashboard/>
+            </Layout>
+          </Sidebar>
+        } />
       </Routes>
     </BrowserRouter>
   );
