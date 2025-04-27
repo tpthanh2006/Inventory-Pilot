024c3e6b2db671b3e85ebfd8e783bada2a9d5828
tpthanh2006 <tranpthanh2006@gmail.com>
2025-04-27 15:35:59 -0500

    monitor password strength

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,7 +1,9 @@
 import {toast} from "react-toastify"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import { FaTimes } from 'react-icons/fa'
+import { BsCheck2All } from 'react-icons/bs'
 import { TiUserAddOutline } from 'react-icons/ti'
 
 import styles from './auth.module.scss'
@@ -24,11 +26,58 @@ const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState(initialState);
   const {name, email, password, password2} = formData;
+  
+  const [uCase, setUCase] = useState(false);
+  const [num, setNum] = useState(false);
+  const [sChar, setSChar] = useState(false);
+  const [passLength, setPassLength] = useState(false);
+
+  const timesIcon = <FaTimes color="red" size={15} />;
+  const checkIcon = <BsCheck2All color="green" size={15} />;
+
+  const switchIcon = (condition) => {
+    if (condition) {
+      return checkIcon;
+    };
+
+    return timesIcon;
+  };
 
   const handleInputChange = async (e) => {
     const {name, value} = e.target;
     setFormData({...formData, [name]: value});
   };
+
+  // Handle password validation
+  useEffect(() => {
+    // Check lower & upper case
+    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+      setUCase(true);
+    } else {
+      setUCase(false);
+    }
+
+    //  Check numbers
+    if (password.match(/([0-9])/)) {
+      setNum(true);
+    } else {
+      setNum(false);
+    }
+
+    //  Check special characters
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+      setSChar(true);
+    } else {
+      setSChar(false);
+    }
+
+    //  Check password length
+    if (password.length > 5) {
+      setPassLength(true);
+    } else {
+      setPassLength(false);
+    }
+  }, [password])
 
   const register = async (e) => {
     e.preventDefault();
@@ -114,6 +163,40 @@ const Register = () => {
               value={password2}
               onChange={handleInputChange}
             />
+
+            {/* Password Strength */}
+            <Card cardClass={styles.group}>
+              <ul className="form-list"> 
+                <li>
+                  <span className={styles.indicator}>
+                    {switchIcon(uCase)}
+                    &nbsp; Lowercase & Uppercase
+                  </span>
+                </li>
+
+                <li>
+                  <span className={styles.indicator}>
+                    {switchIcon(num)}
+                    &nbsp; Numbers (0 - 9)
+                  </span>
+                </li>
+
+                <li>
+                  <span className={styles.indicator}>
+                    {switchIcon(sChar)}
+                    &nbsp; Special Characters (!@#$%^&*)
+                  </span>
+                </li>
+
+                <li>
+                  <span className={styles.indicator}>
+                    {switchIcon(passLength)}
+                    &nbsp; At least 6 Characters
+                  </span>
+                </li>
+              </ul>
+            </Card>
+
             <button type="submit" className="--btn --btn-primary --btn-block">
               Register
             </button>
