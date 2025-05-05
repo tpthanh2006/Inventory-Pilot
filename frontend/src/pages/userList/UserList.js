f0da0e9320a273569a003159972fcfad698b7b40
tpthanh2006 <tranpthanh2006@gmail.com>
2025-05-04 20:30:56 -0500

    set up user list page

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
 
 import './UserList.scss'
 import UserStats from '../userStats/UserStats'
@@ -13,18 +14,50 @@ const UserList = () => {
       <UserStats />
 
       <div className='user-list'>
-        <div className='--flex-between'>
-          <span>
-            <h3 className='--mt'>All Users</h3>
-          </span>
+        <div className='table'>
+          <div className='--flex-between'>
+            <span>
+              <h3 className='--mt'>All Users</h3>
+            </span>
 
-          <span>
-            <Search
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder={"Search users"}
-            />
-          </span>
+            <span>
+              <Search
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder={"Search users"}
+              />
+            </span>
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Change Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Phu Thanh Tran</td>
+                <td>tranpthanh2006@gmail.com</td>
+                <td>Admin</td>
+                <td>
+                  Change Role
+                </td>
+                <td>
+                  <span>
+                    <FaTrashAlt color='red' size={20}/>
+                  </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
